9b8127297ea53d80213a0cae23a14419b08f8cb5
sistema-de-votacao-urna-digital-br <vargas.anthony0725@gmail.com>
2026-08-14 21:34:48 -0300

    Create menu.js

---
+++ menu.js
@@ -0,0 +1,11 @@
+// =========================================================================
+// FUNÇÕES DE NAVEGAÇÃO DO MENU
+// =========================================================================
+
+function irParaVotacao() {
+    window.location.href = "votacao.html";
+}
+
+function irParaResultados() {
+    window.location.href = "resultados.html";
+}
