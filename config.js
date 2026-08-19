9698d2108c3914e8c0cafcfb852f20eebb0a5e31
sistema-de-votacao-urna-digital-br <vargas.anthony0725@gmail.com>
2026-08-18 22:24:52 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,1 @@
-const URL_APPS_SCRIPT = "https://script.google.com/macros/s/AKfycbwnbjc2v49u3x5FhqG6bGTGoeXGCCak6Gerz_m9BrBFW68yOx1C1P_sXNHE00cFHrs0/exec";
+const URL_APPS_SCRIPT = "https://script.google.com/macros/s/AKfycbyHxoBCzdMLsqNHOazSkazLDde-MfjMT4NJM-t0WzLE8vkXZx6r5egOV3PVYpxgmqat/exec";
